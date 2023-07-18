5818310c64998fa8a5c77a0b8ced87ef974c8bba
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-07-18 13:54:03 -0300

    Add files via upload

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,10 +1,14 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Search({ animes }) {
   const router = useRouter();
 
   return (
-    <main>
-      <h1>Search other</h1>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between p-24 bg-zinc-800 text-zinc-200">
+      <h1 className="text-xl text-bold uppercase mb-4">Search other</h1>
       <form
+        className="w-full my-8"
         onSubmit={(event) => {
           event.preventDefault();
           event.stopPropagation();
@@ -16,22 +20,30 @@ export default function Search({ animes }) {
         }}
       >
         <input
+          className="w-full rounded-md py-2 px-4 outline-none border-2 border-white focus:border-red-300 text-zinc-900"
           type="search"
           placeholder="Search anime..."
         />
       </form>
 
-      <h1>Results</h1>
+      <h1 className="text-xl text-bold uppercase mb-4">Results</h1>
 
-      <ul>
+      <ul className="flex flex-wrap gap-4 items-center">
         {animes?.map((item) => (
-          <li key={item?.category_id}>
-            <img
-              src={`https://cdn.appanimeplus.tk/img/${item?.category_icon}`}
-              alt="Anime Banner"
-            />
-            <p>{item?.category_name}</p>
-          </li>
+          <Link
+            className="bg-zinc-200/20 rounded-sm transition-all hover:bg-zinc-600 hover:shadow-md"
+            key={item?.category_id}
+            href={`/anime/${item?.category_id}`}
+          >
+            <li className="flex flex-col justify-between w-[200px]">
+              <img
+                className="object-contain w-full"
+                src={`https://cdn.appanimeplus.tk/img/${item?.category_icon}`}
+                alt="Anime Banner"
+              />
+              <p className="p-4 text-zinc-200 text-md">{item?.category_name}</p>
+            </li>
+          </Link>
         ))}
       </ul>
     </main>
@@ -40,7 +52,9 @@ export default function Search({ animes }) {
 
 export async function getServerSideProps(props) {
   const query = props.query.q;
-  const res = await fetch(`https://animeland.appanimeplus.tk/videoweb/api.php?action=searchvideo&searchword=${query}`);
+  const res =
+    await fetch(`https://animeland.appanimeplus.tk/videoweb/api.php?action=searchvideo&searchword=${query}
+  `);
   const animes = await res.json();
 
   return { props: { animes } };
