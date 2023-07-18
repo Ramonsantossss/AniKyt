8f73e2d091c683e60e2c227833e013d914ff5059
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-07-18 13:53:30 -0300

    Add files via upload

--- src/pages/anime/[id]/index.js
+++ src/pages/anime/[id]/index.js
@@ -1,22 +1,30 @@
+import { useState } from "react";
+
 export default function Anime({ animeInfo, episodes }) {
   const [showVideo, setShowVideo] = useState({ url: "" });
 
   return (
     <>
-      <main>
-        <aside>
-          <h1>{animeInfo?.[0]?.category_name}</h1>
-          <img src={`https://cdn.appanimeplus.tk/img/${animeInfo?.[0]?.category_icon}`} />
+      <main className="flex flex-row h-screen w-full overflow-hidden bg-zinc-800 text-zinc-200">
+        <aside className="h-full w-[300px] bg-zinc-500 p-8">
+          <h1 className="text-zinc-200 text-lg mb-8">
+            {animeInfo?.[0]?.category_name}
+          </h1>
+          <img
+            className="w-full object-contain"
+            src={`https://cdn.appanimeplus.tk/img/${animeInfo?.[0]?.category_icon}`}
+          />
         </aside>
 
-        <section>
-          <header>
-            <h1>Episodes</h1>
+        <section className="h-full w-full bg-zinc-700 p-8 overflow-auto">
+          <header className="w-full bg-zinc-900 shadow-sm p-8">
+            <h1 className="text-zinc-200 text-lg">Episodes</h1>
           </header>
-          <ul>
+          <ul className="w-full">
             {episodes?.map((ep) => (
-              <li key={ep?.video_id}>
+              <li key={ep?.video_id} className="bg-zinc-300/20 my-2 p-4">
                 <button
+                  className="w-full text-left"
                   onClick={() => {
                     setShowVideo({ url: ep?.location || ep?.sdlocation });
                   }}
@@ -28,9 +36,14 @@ export default function Anime({ animeInfo, episodes }) {
           </ul>
 
           {showVideo?.url && (
-            <section>
-              <button onClick={() => setShowVideo()}>❌️</button>
-              <video src={showVideo?.url} controls />
+            <section className="absolute w-screen h-screen p-24 bg-zinc-900/90 top-0 left-0 flex items-center">
+              <button
+                className="absolute top-4 left-4 p-4 rounded-sm text-zinc-400 hover:text-zinc-100 hover:bg-zinc-600 transition-colors"
+                onClick={() => setShowVideo()}
+              >
+                ✘
+              </button>
+              <video className="w-full h-full" src={showVideo?.url} controls />
             </section>
           )}
         </section>
@@ -40,10 +53,15 @@ export default function Anime({ animeInfo, episodes }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`https://animeland.appanimeplus.tk/videoweb/api.php?action=viewcategory&categoryid=${context.query.id}`);
+  const res = await fetch(
+    `https://animeland.appanimeplus.tk/videoweb/api.php?action=viewcategory&categoryid=${context.query.id}`
+  );
   const animeInfo = await res.json();
 
-  const episodesResponse = await fetch(`https://animeland.appanimeplus.tk/videoweb/api.php?action=category_videos&category_id=${context.query.id}`);
+  const episodesResponse = await fetch(
+    `https://animeland.appanimeplus.tk/videoweb/api.php?action=category_videos&category_id=${context.query.id}`
+  );
+
   const episodes = await episodesResponse.json();
 
   return { props: { animeInfo, episodes } };
