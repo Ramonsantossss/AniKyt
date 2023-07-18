11a9af1bd13c5e491609f1e83659b0c7fa7287b3
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-07-18 15:42:55 -0300

    Add files via upload

--- src/pages/anime/[id]/index.js
+++ src/pages/anime/[id]/index.js
@@ -1,30 +1,31 @@
 import { useState } from "react";
+import styles from "./anime.module.css";
 
 export default function Anime({ animeInfo, episodes }) {
   const [showVideo, setShowVideo] = useState({ url: "" });
 
   return (
     <>
-      <main className="flex flex-row h-screen w-full overflow-hidden bg-zinc-800 text-zinc-200">
-        <aside className="h-full w-[300px] bg-zinc-500 p-8">
-          <h1 className="text-zinc-200 text-lg mb-8">
+      <main className={styles.main}>
+        <aside className={styles.sidebar}>
+          <h1 className={styles.categoryName}>
             {animeInfo?.[0]?.category_name}
           </h1>
           <img
-            className="w-full object-contain"
+            className={styles.categoryIcon}
             src={`https://cdn.appanimeplus.tk/img/${animeInfo?.[0]?.category_icon}`}
           />
         </aside>
 
-        <section className="h-full w-full bg-zinc-700 p-8 overflow-auto">
-          <header className="w-full bg-zinc-900 shadow-sm p-8">
-            <h1 className="text-zinc-200 text-lg">Episodes</h1>
+        <section className={styles.episodeSection}>
+          <header className={styles.episodeHeader}>
+            <h1 className={styles.episodeTitle}>Episodes</h1>
           </header>
-          <ul className="w-full">
+          <ul className={styles.episodeList}>
             {episodes?.map((ep) => (
-              <li key={ep?.video_id} className="bg-zinc-300/20 my-2 p-4">
+              <li key={ep?.video_id} className={styles.episodeItem}>
                 <button
-                  className="w-full text-left"
+                  className={styles.episodeButton}
                   onClick={() => {
                     setShowVideo({ url: ep?.location || ep?.sdlocation });
                   }}
@@ -36,14 +37,14 @@ export default function Anime({ animeInfo, episodes }) {
           </ul>
 
           {showVideo?.url && (
-            <section className="absolute w-screen h-screen p-24 bg-zinc-900/90 top-0 left-0 flex items-center">
+            <section className={styles.videoSection}>
               <button
-                className="absolute top-4 left-4 p-4 rounded-sm text-zinc-400 hover:text-zinc-100 hover:bg-zinc-600 transition-colors"
+                className={styles.closeButton}
                 onClick={() => setShowVideo()}
               >
                 ✘
               </button>
-              <video className="w-full h-full" src={showVideo?.url} controls />
+              <video className={styles.video} src={showVideo?.url} controls />
             </section>
           )}
         </section>
