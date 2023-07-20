cf5f224c22f1b040e3db8e0f678acbd76d6b3622
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-07-20 08:34:45 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,34 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './home.module.css';
-//import Carousel from './components/Carousel';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Home({ animes }) {
   const router = useRouter();
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Ajuste o número de slides mostrados em diferentes tamanhos de tela conforme necessário
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <main className={`${styles.main}`}>
@@ -30,25 +54,20 @@ export default function Home({ animes }) {
       </form>
       <h1 className={`${styles.title}`}>Populares</h1>
 
-      <ul className={`${styles.list}`}>
+      <Slider {...settings}>
         {animes?.map((item) => (
-          <Link
-            className={`${styles.link}`}
-            key={item?.id}
-            href={`/anime/${item?.id}`}
-          >
-            <li className="flex flex-col justify-between w-[200px]">
+          <Link className={`${styles.link}`} key={item?.id} href={`/anime/${item?.id}`}>
+            <div className="flex flex-col justify-between w-[200px]">
               <img
                 className={`${styles.image}`}
                 src={`https://cdn.appanimeplus.tk/img/${item?.category_icon}`}
-                alt="Anime Banner"
+                alt="Banner do Anime"
               />
               <p className={`${styles.text}`}>{item?.category_name}</p>
-            </li>
+            </div>
           </Link>
         ))}
-      </ul>
-
+      </Slider>
     </main>
   );
 }
