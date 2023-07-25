5a8c927c98e1ac59ad0204294ba29d78ce10be43
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-07-25 13:49:38 -0300

    Add files via upload

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./searchStyles.css"; // Import the CSS file
 
 export default function Search({ animes }) {
   const router = useRouter();
 
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between p-24 bg-zinc-800 text-zinc-200">
-      <h1 className="text-xl text-bold uppercase mb-4">Search other</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Search other</h1>
       <form
-        className="w-full my-8"
+        className={styles.form}
         onSubmit={(event) => {
           event.preventDefault();
           event.stopPropagation();
@@ -20,28 +21,28 @@ export default function Search({ animes }) {
         }}
       >
         <input
-          className="w-full rounded-md py-2 px-4 outline-none border-2 border-white focus:border-red-300 text-zinc-900"
+          className={styles.input}
           type="search"
           placeholder="Search anime..."
         />
       </form>
 
-      <h1 className="text-xl text-bold uppercase mb-4">Results</h1>
+      <h1 className={styles.resultsTitle}>Results</h1>
 
-      <ul className="flex flex-wrap gap-4 items-center">
+      <ul className={styles.results}>
         {animes?.map((item) => (
           <Link
-            className="bg-zinc-200/20 rounded-sm transition-all hover:bg-zinc-600 hover:shadow-md"
+            className={styles.resultItem}
             key={item?.category_id}
             href={`/anime/${item?.category_id}`}
           >
-            <li className="flex flex-col justify-between w-[200px]">
+            <li className={styles.resultItem}>
               <img
-                className="object-contain w-full"
+                className={styles.resultImg}
                 src={`https://cdn.appanimeplus.tk/img/${item?.category_icon}`}
                 alt="Anime Banner"
               />
-              <p className="p-4 text-zinc-200 text-md">{item?.category_name}</p>
+              <p className={styles.resultText}>{item?.category_name}</p>
             </li>
           </Link>
         ))}
@@ -52,9 +53,7 @@ export default function Search({ animes }) {
 
 export async function getServerSideProps(props) {
   const query = props.query.q;
-  const res =
-    await fetch(`https://animeland.appanimeplus.tk/videoweb/api.php?action=searchvideo&searchword=${query}
-  `);
+  const res = await fetch(`https://animeland.appanimeplus.tk/videoweb/api.php?action=searchvideo&searchword=${query}`);
   const animes = await res.json();
 
   return { props: { animes } };
